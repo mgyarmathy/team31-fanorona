1b32d86566dbbf4075af05018e23228ebe41740d
mgyarmathy <mgyarmathy2011@gmail.com>
2013-03-20 20:12:47 -0500

    added rules of fanarona dialogue box

--- README.txt
+++ README.txt
@@ -3,14 +3,12 @@ team31-fanorona
 
 CSCE 315 Team 31 - Fanorona Game
 
-* Michael Gy12343y - 832.641.7741 - mgyarmathy@tamu.edu
+* Michael Gyarmathy - 832.641.7741 - mgyarmathy@tamu.edu
 * David Harrison - 936.349.6230 - metace13@tamu.edu
 * Jeffrey Foss - 254.291.4986 - jdfwildman@tamu.edu
 
-INSTALL ECLIPSE
-http://www.eclipse.org/downloads/
-
-Seen by David Harrison
+Excellent Fanarona Game for reference:
+http://rescomp.stanford.edu/~domingo2/stuff/fanorona_java.html
 
 
 http://www.tutorialspoint.com/java/java_basic_syntax.htm
--- src/Fanorona.java
+++ src/Fanorona.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 public class Fanorona extends JFrame{
 	
-	public GamePanel board;
+	GamePanel board;
 	
 	
 	public Fanorona(){
@@ -26,17 +26,38 @@ public class Fanorona extends JFrame{
 		JMenu fileMenu = new JMenu("File");
 		JMenuItem newGame = new JMenuItem("New Game");
 		newGame.addActionListener(new ActionListener() {
-			 
             public void actionPerformed(ActionEvent e)
             {
                 board.newGame();
             }
         });
+		
+		JMenuItem help = new JMenuItem("How To Play");
+		help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+            	JOptionPane.showMessageDialog(null,
+            			"How to Play Fanarona" +
+            			"\n- Perform moves by selecting a piece and selecting an open adjacent space to move to." +
+            			"\n- There are two kinds of moves, non-capturing and capturing moves. " +
+            			"\n- A non-capturing move involves moving a piece along a line to an adjacent space." +
+            			"\n- Capturing moves can be made in two different ways to remove one or more of the opponent's pieces:" +
+            			"\n   1. By approach -- moving the capturing piece to a point adjacent to an opponent piece." +
+            			"\n   2. By withdrawal -- moving in the opposite direction of an adjacent piece." +
+            			"\n- When an opponent stone is captured, all opponent pieces in line behind that stone are captured as well." +
+            			"\n- A capturing piece is allowed to continue making successive captures, with the following restrictions:" +
+            			"\n   1. The piece is not allowed to arrive at the same position twice." +
+            			"\n   2. It is not allowed to move a piece in the same direction as directly before in the capturing sequence." +
+            			"\n- A capturing move must be made if possible, otherwise the player can make a non-capturing move." +
+            			"\n- The game ends when one player captures all stones of the opponent. " +
+            			"\n- If neither player can achieve this within 50 turns, the game is a draw", 
+            			"Team 31 - Fanarona",
+            			JOptionPane.PLAIN_MESSAGE);
+            }
+        });
 		fileMenu.add(newGame);
+		fileMenu.add(help);
 		menuBar.add(fileMenu);
-		menuBar.add(Box.createHorizontalGlue());
-		JMenu help = new JMenu("Help");
-		menuBar.add(help);
 		menuBar.setBackground(Color.WHITE);
 		menuBar.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 		setJMenuBar(menuBar);
